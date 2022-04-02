78b05a14fc67091de94bad9f945a160528912039
balbino aylagas <balbino_a@hotmail.com>
2022-04-02 11:08:52 -0600

    modify config 02

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ class BaseConfig(object):
     # postgresql://scott:tiger@localhost:5432/mydatabase
     SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost:3306/testdb"
     """ SQLALCHEMY_DATABASE_URI = "sqlite:///todolist_database.db" """
-    """ SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://root:root@localhost:5432/testdb" """
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     """
     SQLALCHEMY_POOL_SIZE = 20
