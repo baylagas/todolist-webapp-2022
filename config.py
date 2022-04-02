87706d937246a9545f23303ba5b5a5c6e52269c7
balbino aylagas <balbino_a@hotmail.com>
2022-04-02 11:18:16 -0600

    modify config 05

--- config.py
+++ config.py
@@ -2,9 +2,7 @@ class BaseConfig(object):
     # sqlite:////tmp/test.db
     # postgresql://scott:tiger@localhost:5432/mydatabase
     """SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost:3306/testdb" """
-    SQLALCHEMY_DATABASE_URI = (
-        "mysql://b96354f915678f:6ebe6dfb@us-cdbr-east-05.cleardb.net:3306/heroku_e3185dc57eef12c?reconnect=true"
-    )
+    SQLALCHEMY_DATABASE_URI = "mysql://b96354f915678f:6ebe6dfb@us-cdbr-east-05.cleardb.net:3306/heroku_e3185dc57eef12c"
     """ SQLALCHEMY_DATABASE_URI = "sqlite:///todolist_database.db" """
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     """
