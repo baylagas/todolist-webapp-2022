a6668d5bf06c3e6ff4c87e39417e9ca10261fa0a
balbino aylagas <balbino_a@hotmail.com>
2022-04-02 11:59:21 -0600

    modify config 08

--- config.py
+++ config.py
@@ -5,6 +5,19 @@ class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = "mysql://b96354f915678f:6ebe6dfb@us-cdbr-east-05.cleardb.net:3306/heroku_e3185dc57eef12c"
     """ SQLALCHEMY_DATABASE_URI = "sqlite:///todolist_database.db" """
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    """ SQLALCHEMY_POOL_SIZE = 20
+    SQLALCHEMY_POOL_TIMEOUT = 300 """
+    SECRET_KEY = "horrible_secret_key"
+    LOGIN_DISABLED = True
+
+
+class ProductionConfig(object):
+    # sqlite:////tmp/test.db
+    # postgresql://scott:tiger@localhost:5432/mydatabase
+    """SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost:3306/testdb" """
+    SQLALCHEMY_DATABASE_URI = "mysql://b96354f915678f:6ebe6dfb@us-cdbr-east-05.cleardb.net:3306/heroku_e3185dc57eef12c"
+    """ SQLALCHEMY_DATABASE_URI = "sqlite:///todolist_database.db" """
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_POOL_SIZE = 20
     SQLALCHEMY_POOL_TIMEOUT = 300
     SECRET_KEY = "horrible_secret_key"
