8cefbd98ea02f74565d772348736a222f2d48536
balbino aylagas <balbino_a@hotmail.com>
2022-04-02 11:21:33 -0600

    modify config 06

--- config.py
+++ config.py
@@ -5,9 +5,7 @@ class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = "mysql://b96354f915678f:6ebe6dfb@us-cdbr-east-05.cleardb.net:3306/heroku_e3185dc57eef12c"
     """ SQLALCHEMY_DATABASE_URI = "sqlite:///todolist_database.db" """
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    """
     SQLALCHEMY_POOL_SIZE = 20
     SQLALCHEMY_POOL_TIMEOUT = 300
-    """
     SECRET_KEY = "horrible_secret_key"
     LOGIN_DISABLED = True
